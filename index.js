d91f67bd5827ca1dc0ef12ee8928cb34ab0eb4be
Steven Mendoza <smendoza171@gmail.com>
2022-07-29 02:31:05 -0700

    reorganized

--- index.js
+++ index.js
@@ -9,17 +9,36 @@ const colorInput = document.getElementById("colorInput");
 const modeInput = document.getElementById("modeInput");
 const numColorsInput = document.getElementById("numColorsInput");
 
+colorInput.addEventListener("change", ()=> {
+  hexInput.value = colorInput.value;
+  renderInputColor();
+})
+
+
+function renderInputColor() {
+    sliderInput.value = 25;
+    sliderText.innerText = `25%`;
+
+    let inputHex = hexInput.value;
+    inputColor.style.backgroundColor = inputHex;
+    inputColorText.innerText = `Input Color ${inputHex}`;
+
+    let alteredHex = alterColor(inputHex, 25);
+    alteredColor.style.backgroundColor = alteredHex;
+    alteredColorText.innerText = `Altered Color ${alteredHex}`; 
+}
+
 
 let numColors = 5;
 const modes = [
+  "analogic",
+  "analogic-complement",
   "monochrome",
   "monochrome-dark",
   "monochrome-light",
-  "analogic",
-  "analogic-complement",
   "complement",
   "triad",
-  "quad"
+  "quad",
 ];
 const base = "https://www.thecolorapi.com/";
 const endpoint = "scheme";
@@ -52,28 +71,44 @@ function getScheme() {
         renderColors(colorsArray);
     })
 }
-
+colorDisplay = document.getElementById("colorDisplay");
 
 function renderColors(colors) {
-    let html = ""
+    let render_html = ``
     for (let color of colors) {
-      html += `
-            <button class="color-btn" onclick="copyColor(this)">
-            <div class="color-col">
+      render_html += `
+            <div class="color-col" onclick="copyColor('${color.hex.value}')">
                 <div class="color" style="background-color:${color.hex.value}"></div>
                 <p class="color-code">${color.hex.value}</p>
             </div>
-            </button>
         `;
     }
-    document.getElementById("colorDisplay").innerHTML = html;
+    colorDisplay.innerHTML = render_html;
 }
 
 
+let includeHash = false;
+
+toggleHashBtn = document.getElementById("toggleHashBtn");
+toggleHashBtn.addEventListener("click", ()=> {
+  includeHash = !includeHash;
+  if (includeHash) {
+    toggleHashBtn.style.backgroundColor = "#14aaaa";
+  } else {
+    toggleHashBtn.style.backgroundColor = "#14459A";
+  }
+})
+
+
+
 function copyColor(color) {
-    var copyText = color.innerText.replace("#", "");
-    // var copyText = color.innerText;
-    navigator.clipboard.writeText(copyText);
+  color = color.replace("#", "");
+
+  if (!includeHash) {copyText = color;}
+  else {
+    copyText = "#" + color;
+  };
+  navigator.clipboard.writeText(copyText);
 }
 
 
@@ -106,32 +141,36 @@ toggleBtn.addEventListener('click', () => {
     lightenText.classList.add('unselected');
     darkenText.classList.remove('unselected');
   }
-  reset(); 
+  // reset(); 
+  renderInputColor();
 })
 
 hexInput.addEventListener('keyup', () => {
-  
-  const hex = hexInput.value;
+    const hex = hexInput.value;
   if(!isValidHex(hex)) return;
-  
-  const strippedHex = hex.replace('#', '');
-
+    const strippedHex = hex.replace('#', '');
   inputColor.style.backgroundColor = "#" + strippedHex; 
+
+  console.log();
+  colorInput.value = "#" + strippedHex;
+
   reset(); 
 })
 
-const isValidHex = (hex) => {
-    if(!hex) return false;
-    
-    const strippedHex = hex.replace('#', '');
-    return strippedHex.length === 3 || strippedHex.length === 6;
+
+
+
+
+
+function isValidHex(hex) {
+  if (!hex) return false;
+  const strippedHex = hex.replace("#", "");
+  return strippedHex.length === 3 || strippedHex.length === 6;
 }
 
 
-
-const convertHexToRGB = (hex) => {
+function convertHexToRGB(hex) {
   if(!isValidHex(hex)) return null;
-  
   let strippedHex = hex.replace('#', '');
 
   if(strippedHex.length === 3){
@@ -139,39 +178,35 @@ const convertHexToRGB = (hex) => {
     + strippedHex[1] + strippedHex[1] 
     + strippedHex[2] + strippedHex[2];
   }
-  
   const r  = parseInt(strippedHex.substring(0,2), 16);
   const g  = parseInt(strippedHex.substring(2,4), 16);
   const b  = parseInt(strippedHex.substring(4,6), 16);
-  
   return {r,g,b};
 }
 
-const convertRGBToHex = (r,g,b) => {
+function convertRGBToHex(r,g,b) {
   const firstPair = ("0" + r.toString(16)).slice(-2);
   const secondPair = ("0" + g.toString(16)).slice(-2);
   const thirdPair = ("0" + b.toString(16)).slice(-2);
-  
   const hex = "#" + firstPair + secondPair + thirdPair;
   return hex;
 }
 
-const alterColor = (hex, percentage) => {
-  const {r, g, b} = convertHexToRGB(hex);
-  
-  const amount = Math.floor((percentage/100) * 255);
-  
-  const newR = increaseWithin0To255(r,amount);
-  const newG = increaseWithin0To255(g,amount);
-  const newB = increaseWithin0To255(b,amount)
+function alterColor(hex, percentage) {
+  const { r, g, b } = convertHexToRGB(hex);
+  const amount = Math.floor((percentage / 100) * 255);
+  const newR = increaseWithin0To255(r, amount);
+  const newG = increaseWithin0To255(g, amount);
+  const newB = increaseWithin0To255(b, amount);
   return convertRGBToHex(newR, newG, newB);
 }
 
-const increaseWithin0To255 = (hex, amount) => {
-  return Math.min(255, Math.max(0, hex + amount));
+
+function increaseWithin0To255(hex, amount) {
+    return Math.min(255, Math.max(0, hex + amount));
 }
 
-alterColor("fff", 10)
+
 
 sliderInput.addEventListener('input', () => {
   if(!isValidHex(hexInput.value)) return;
@@ -187,16 +222,88 @@ sliderInput.addEventListener('input', () => {
   alteredColorText.innerText = `Altered Color ${alteredHex}`; 
 })
 
-const reset = () =>{ 
-  sliderInput.value = 0;
-  sliderText.innerText=`0%`;
-  alteredColor.style.backgroundColor = hexInput.value;
-  alteredColorText.innerText = `Altered Color ${hexInput.value}`; 
+
+
+function reset() {
+  sliderInput.value = 25;
+  sliderText.innerText = `25%`;
+
+  let inputHex = hexInput.value;
+  inputColor.style.backgroundColor = inputHex;
+  inputColorText.innerText = `Input Color ${inputHex}`; 
+
+
+  let alteredHex = alterColor(inputHex, 25);
+  alteredColor.style.backgroundColor = alteredHex;
+  alteredColorText.innerText = `Altered Color ${alteredHex}`; 
+}
+
+
+
+
+
+
+
+
+function roll(min, max, floatFlag) {
+    let r = Math.random() * (max - min) + min
+    return floatFlag ? r : Math.floor(r)
+}
+
+
+function generateRandomColor() {
+  let r = roll(0, 256, 0);
+  let g = roll(0, 256, 0);
+  let b = roll(0, 256, 0);
+  let hex = convertRGBToHex(r,g,b);
+  console.log(hex);
+  hexInput.value = hex;
+  colorInput.value = hex;
+  
+
+  inputColor.style.backgroundColor = hexInput.value;
+  console.log(colorInput.value);
+
+  let alteredHex =  alterColor(hexInput.value, 25);
+  alteredColor.style.backgroundColor = alteredHex;
+getScheme();
+
+  reset();
+
+
 
 }
 
 
 
-// Start with random color
+
+const randomColorBtn = document.getElementById("randomColorBtn");
+randomColorBtn.addEventListener("click", generateRandomColor);
+reset();
+generateRandomColor();
+
+
+inputColor.addEventListener("click", ()=> {
+  console.log(colorInput.value);
+  copyColor(colorInput.value);
+})
+// alteredColor.addEventListener("click", () => {
+//   console.log(colorInput.value);
+//   copyColor(colorInput.value);
+// });
+
+
+
+
+
+
+
+
+// generate random theme, num colors and mode
+// copy altered color to clipboard
+// upate hex input when color chosen and rerender
+//create general render function
+
 // chrome extension
 // valid hex character check
+// add rgb sliders for each value
